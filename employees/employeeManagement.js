e70d4aa1e031bb5774e5501a5f4f95ccf817b6fe
Cleggatron <david.robert.clegg@gmail.com>
2021-08-26 13:42:13 +0100

    renamed file due to spelling error.

--- employees/employeeManagement.js
+++ employees/employeeManagement.js
@@ -1,10 +1,29 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
 
 class EmployeeManagement {
     constructor () {
+        this.db = "";
+    }
+    startConnection(){
+        this.db = mysql.createConnection(
+            {
+                host: "localhost",
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME
+            },
+            console.log("Connected to the database.")
+        )
+
+        this.startManagement()
     }
 
     startManagement(){
+       
         this.employeeOptions();
     }
 
