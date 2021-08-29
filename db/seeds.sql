96f8a3f9c4609ab1f180c929bd1626346ebe72ff
Cleggatron <david.robert.clegg@gmail.com>
2021-08-29 19:48:24 +0100

    updated seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,12 +13,12 @@ INSERT INTO role (title, salary, department_id) VALUES
     
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ("John", "Smith", 1, 2),
-    ("Johannes", "Smyth", 1, NULL),
-    ("Jane", "Smith", 2, 4),
-    ("Janet", "Smyth", 2, NULL),
-    ("James", "Smith", 3, 6),
-    ("Jemima", "Smyth", 3, NULL);
+    ("Arthur", "Dent", 1, 2),
+    ("Zaphod", "Beelblebrox", 1, NULL),
+    ("Trisha", "Mcmillan", 2, 4),
+    ("Slarti", "Bartfarst", 2, NULL),
+    ("Marvin", "Android", 3, 6),
+    ("Fenchurch", "StreetStation", 3, NULL);
 
 
 
